0f827242046aa67d66eaae1d50be3a8ad51a0123
Asmita Singh <asmita.singh1710@gmail.com>
2025-07-29 15:09:49 +0530

    updating cors policy

--- app/cors.py
+++ app/cors.py
@@ -2,7 +2,6 @@ import os
 from flask_cors import CORS
 
 def init_cors(app):
-    app.config['CORS_ORIGINS'] = ['*']
+    app.config['CORS_ORIGINS'] = ['http://localhost:3000', 'https://voicruit.com']
     app.config['CORS_METHODS'] = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
-    app.config['CORS_EXPOSE_HEADERS'] = ['access-token']
     CORS(app)
